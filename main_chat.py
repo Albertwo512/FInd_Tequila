c0b5b3b5312e59979092cd07192c3fda2e9f7135
Albertwo512 <122117432+Albertwo512@users.noreply.github.com>
2025-05-08 16:47:14 -0600

    actualizacion

--- main_chat.py
+++ main_chat.py
@@ -52,7 +52,7 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["openai"]["openai_api_key"])
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
